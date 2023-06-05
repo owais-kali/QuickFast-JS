879d70b588c14f371316174d12a5d296fd5e9ff9
owais-kali <sh.owais66@gmail.com>
2023-06-05 08:58:44 +0530

    Update!

--- tests/boost-test/boost-test.cpp
+++ tests/boost-test/boost-test.cpp
@@ -1,4 +1,4 @@
-//#define CLION
+// #define CLION
 
 #ifndef CLION
 #include <emscripten.h>
@@ -15,6 +15,10 @@
 #include <Codecs/SingleMessageConsumer.h>
 #include <Messages/MessageAccessor.h>
 
+#include "template_consumer.h"
+
+extern "C" {
+
 EMSCRIPTEN_KEEPALIVE
 QuickFAST::Codecs::TemplateRegistryPtr
 parse_templates(const std::string &template_filename) {
@@ -23,19 +27,84 @@ parse_templates(const std::string &template_filename) {
   return parser.parse(template_stream);
 }
 
-EMSCRIPTEN_KEEPALIVE
-void test(){
-  parse_templates("");
-  printf("test function called!\n");
+size_t ReadDataFromFile(const char* path, char** out){
+  // Open the binary file for reading
+  std::ifstream inFile(path, std::ios::binary);
+  if (!inFile) {
+    throw std::runtime_error("Failed to open the file for reading.");
+  }
 
-  QuickFAST::Codecs::TemplateRegistryPtr templates_(nullptr);
+  // Determine the file size
+  inFile.seekg(0, std::ios::end);
+  std::streampos fileSize = inFile.tellg();
+  inFile.seekg(0, std::ios::beg);
+
+  // Allocate memory to store the file data
+  char* buffer = new char[fileSize];
+
+  // Read the file data into the buffer
+  inFile.read(buffer, fileSize);
+
+  // Check if the read operation was successful
+  if (!inFile) {
+    std::cerr << "Failed to read the binary data from the file." << std::endl;
+    delete[] buffer;
+    throw std::runtime_error("Failed to read the binary data from the file.");
+  }
+
+  *out = buffer;
+  // Close the file
+  inFile.close();
+
+  return fileSize;
+
+  std::cout << "Binary data was successfully read from the file." << std::endl;
+}
+
+const char* cstr(const std::string& message) {
+  char * cstr = new char [message.length()+1];
+  std::strcpy (cstr, message.c_str());
+  return cstr;
+}
+
+EMSCRIPTEN_KEEPALIVE
+const char* getAMessage() {
+  return cstr("oh hai there!");
+};
+
+EMSCRIPTEN_KEEPALIVE
+const char* test(){
+  QuickFAST::Codecs::TemplateRegistryPtr templates_(parse_templates("/media/owais/LinuxHDD1/Freelancer/BitZee/TradeEngine/Tests/QuickFast_Decoder_Test/input/Candle.xml"));
   QuickFAST::Codecs::Decoder decoder_(templates_);
 
-  QuickFAST::Codecs::DataSourceBuffer source(nullptr, 0);
+  const unsigned char* encoded_data;
+  auto size = ReadDataFromFile("/media/owais/LinuxHDD1/Freelancer/BitZee/TradeEngine/Tests/QuickFast_Decoder_Test/input/encoded_data.bin",
+                               (char **)&encoded_data);
+
+  // Decode the message
+  QuickFAST::Codecs::DataSourceBuffer source(encoded_data, size);
   QuickFAST::Codecs::SingleMessageConsumer consumer;
   QuickFAST::Codecs::GenericMessageBuilder builder(consumer);
   decoder_.decodeMessage(source, builder);
   QuickFAST::Messages::Message& msg(consumer.message());
+
+  // Examine message contents
+  uint64_t seq_num, msg_type, timestamp;
+  const QuickFAST::StringBuffer* string_buffer;
+  std::string symbol;
+
+  if (!msg.getUnsignedInteger(TemplateConsumer::id_seq_num_, QuickFAST::ValueType::UINT32, seq_num)) {
+    std::cout << "Could not get seq num from msg" << std::endl;
+  }
+
+  if (!msg.getString(TemplateConsumer::id_symbol_, QuickFAST::ValueType::ASCII, string_buffer)) {
+    std::cout << "Could not get symbol from msg" << std::endl;
+  }
+  symbol = (std::string)*string_buffer;
+
+  delete encoded_data;
+
+  return cstr("symbol");
 }
 
 EMSCRIPTEN_KEEPALIVE
@@ -56,3 +125,5 @@ int fib(int n) {
   }
   return b;
 }
+
+} //extern "C" END
