4f741640d567999828c0a4feee29f72b7df8575f
owais-kali <sh.owais66@gmail.com>
2023-06-06 12:13:45 +0530

    Test: Create Decoder

---
+++ tests/create-decoder/template_consumer.cpp
@@ -0,0 +1,25 @@
+
+#include <boost/cstdint.hpp>
+#include "template_consumer.h"
+#include <fstream>
+#include <Codecs/XMLTemplateParser.h>
+
+using namespace QuickFAST::Messages;
+
+const FieldIdentity TemplateConsumer::id_seq_num_("SequenceNumber");
+const FieldIdentity TemplateConsumer::id_timestamp_("Timestamp");
+const FieldIdentity TemplateConsumer::id_symbol_("Symbol");
+
+const FieldIdentity TemplateConsumer::id_open_("open");
+const FieldIdentity TemplateConsumer::id_high_("high");
+const FieldIdentity TemplateConsumer::id_low_("low");
+const FieldIdentity TemplateConsumer::id_close_("close");
+const FieldIdentity TemplateConsumer::id_volume_("volume");
+
+QuickFAST::Codecs::TemplateRegistryPtr 
+TemplateConsumer::parse_templates(const std::string& template_filename)
+{
+  std::ifstream template_stream(template_filename.c_str());
+  QuickFAST::Codecs::XMLTemplateParser parser;
+  return parser.parse(template_stream);
+}
